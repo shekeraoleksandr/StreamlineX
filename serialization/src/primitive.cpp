3b18d2963df22dc7b02db1cf8ec99fed244240f2
Oleksandr Shekera <oleksandrshekera@gmail.com>
2023-04-15 15:02:26 +0300

    Bug fix: changed primtive.cpp item type

--- serialization/src/primitive.cpp
+++ serialization/src/primitive.cpp
@@ -6,24 +6,14 @@ namespace ObjectModel
 {
 
 	Primitive::Primitive()
+		: Root()
 	{
 		size += sizeof type;
 	}
 
-	template<typename T>
-	static std::unique_ptr<Primitive> Primitive::create(std::string name, Type type, T value)
+	Primitive::~Primitive()
 	{
-		std::unique_ptr<Primitive> p(new Primitive());
-		p->setName(name);
-		p->wrapper = static_cast<uint8_t>(Wrapper::PRIMITIVE);
-		p->type = static_cast<uint8_t>(type);
-		p->data = new std::vector<uint8_t>(sizeof value);
-		p->size += (int32_t)p->data->size();
-		int16_t iterator = 0;
-		Core::template encode<T>(*p->data, iterator, value);
-
-
-		return p;
+		delete data;
 	}
 
 	void Primitive::pack(std::vector<uint8_t>& buffer, int16_t& iterator)
@@ -35,10 +25,5 @@ namespace ObjectModel
 		Core::encode<uint8_t>(buffer, iterator, *data);
 		Core::encode<int32_t>(buffer, iterator, size);
 
-	}
-
-	std::vector<uint8_t> Primitive::getData()
-	{
-		return *data;
 	}
 }
