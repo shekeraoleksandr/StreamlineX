67b9301679523523cb57e934081a8d2a8cc32636
Oleksandr Shekera <oleksandrshekera@gmail.com>
2023-04-17 11:12:11 +0300

    Created getTypeSize method

--- serialization/src/meta.h
+++ serialization/src/meta.h
@@ -26,4 +26,20 @@ namespace ObjectModel
 
 		BOOL
 	};
+
+	template<typename ...>
+	uint8_t getTypeSize(Type type)
+	{
+		switch (type)
+		{
+		case Type::BOOL: return sizeof(bool); break;
+		case Type::I8: return sizeof(uint8_t); break;
+		case Type::I16: return sizeof(int16_t); break;
+		case Type::I32: return sizeof(int32_t); break;
+		case Type::I64: return sizeof(int64_t); break;
+		case Type::FLOAT: return sizeof(float); break;
+		case Type::DOUBLE: return sizeof(double); break;
+		}
+		return 0;
+	}
 }
