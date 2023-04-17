3e28a41f2818658d194fcf9c91051e29d6d074a1
Oleksandr Shekera <oleksandrshekera@gmail.com>
2023-04-17 14:06:49 +0300

    Changed methods definition *refactor later*

--- serialization/src/object.cpp
+++ serialization/src/object.cpp
@@ -8,42 +8,98 @@ namespace ObjectModel
 	{
 		setName(name);
 		wrapper = static_cast<uint8_t>(Wrapper::OBJECT);
-		size += (sizeof int16_t) * 4;
+		size += (sizeof(int16_t)) * 4;
 	}
 
 	void Object::addEntity(Root* r)
 	{
-		this->entities.push_back(r);
-		count += 1;
+		switch (r->wrapper)
+		{
+		case 1: primitives.push_back(*dynamic_cast<Primitive*>(r)); primitiveCount += 1; break;
+		case 2: arrays.push_back(*dynamic_cast<Array*>(r)); arrayCount += 1; break;
+		case 3: strings.push_back(*dynamic_cast<Array*>(r)); stringCount += 1; break;
+		case 4: objects.push_back(*dynamic_cast<Object*>(r)); objectCount += 1; break;
+		}
+
 		size += r->getSize();
 	}
 
-	void Object::pack(std::vector<uint8_t>& buffer, int16_t& iterator)
-	{
-		Core::encode<std::string>(buffer, iterator, name);
-		Core::encode<int16_t>(buffer, iterator, nameLength);
-		Core::encode<int8_t>(buffer, iterator, wrapper);
-		Core::encode<int16_t>(buffer, iterator, count);
 
-		for (const auto r : entities)
+
+
+	void Object::pack(std::vector<uint8_t>& buffer, int16_t& it)
+	{
+		Core::encode<uint8_t>(buffer, it, wrapper);
+		Core::encode<int16_t>(buffer, it, nameLength);
+		Core::encode<std::string>(buffer, it, name);
+
+		// refactor this into std::vector<Entities*> entities;
+		// for (auto e : entities) {e.pack(b,i};}
+		Core::encode<int16_t>(buffer, it, primitiveCount);
+		for (auto p : primitives)
 		{
-			r->pack(buffer, iterator);
+			p.pack(buffer, it);
 		}
 
-		Core::encode<int32_t>(buffer, iterator, size);
+		Core::encode<int16_t>(buffer, it, arrayCount);
+		for (auto arr : arrays)
+		{
+			arr.pack(buffer, it);
+		}
+
+		Core::encode<int16_t>(buffer, it, stringCount);
+		for (auto str : strings)
+		{
+			str.pack(buffer, it);
+		}
+
+		Core::encode<int16_t>(buffer, it, objectCount);
+		for (auto o : objects)
+		{
+			o.pack(buffer, it);
+		}
+
+
+		Core::encode<int32_t>(buffer, it, size);
 
 	}
 
-	Root* Object::findByName(std::string name)
+	Object Object::unpack(std::vector<uint8_t>& buffer, int16_t& it)
 	{
-		for (const auto r : entities)
+		Object obj;
+		obj.wrapper = Core::decode<uint8_t>(buffer, it);
+		obj.nameLength = Core::decode<int16_t>(buffer, it);
+		obj.name = Core::decode<std::string>(buffer, it);
+
+		// refactor this into:
+		// for (auto e : entities) {e.entities.push_back(e.unpack(b,i));}
+		obj.primitiveCount = Core::decode<int16_t>(buffer, it);
+		for (int i = 0; i < obj.primitiveCount; i++)
 		{
-			if (r->getName() == name)
-			{
-				return r;
-			}
+			obj.primitives.push_back(Primitive::unpack(buffer, it));
 		}
-		std::cout << "no as such" << std::endl;
-		return new Object("ninjia");
+
+		obj.arrayCount = Core::decode<int16_t>(buffer, it);
+		for (int i = 0; i < obj.arrayCount; i++)
+		{
+			obj.arrays.push_back(Array::unpack(buffer, it));
+		}
+
+		obj.stringCount = Core::decode<int16_t>(buffer, it);
+		for (int i = 0; i < obj.stringCount; i++)
+		{
+			obj.strings.push_back(Array::unpackS(buffer, it));
+		}
+
+		obj.objectCount = Core::decode<int16_t>(buffer, it);
+		for (int i = 0; i < obj.objectCount; i++)
+		{
+			obj.objects.push_back(unpack(buffer, it));
+		}
+
+		obj.size = Core::decode<int32_t>(buffer, it);
+
+
+		return obj;
 	}
 }
--- serialization/src/object.h
+++ serialization/src/object.h
@@ -1,21 +1,80 @@
 #pragma once
 #include <iostream>
-#include "root.h"
+#include "primitive.h"
+#include "array.h"
+#include "object.h"
 
 namespace ObjectModel
 {
-	class LIB Object : public Root
+	class Object : public Root
 	{
 	public:
-		std::vector<Root*> entities;
-		int16_t count = 0;
+		int16_t primitiveCount = 0, arrayCount = 0, stringCount = 0, objectCount = 0;
+		std::vector<Primitive> primitives;
+		std::vector<Array> arrays;
+		std::vector<Array> strings;
+		std::vector<Object> objects;
 	public:
 		Object(std::string);
 		void addEntity(Root*);
 		void pack(std::vector<uint8_t>&, int16_t&);
+		static Object unpack(std::vector<uint8_t>&, int16_t&);
+		inline int16_t getPrimitiveCount() { return primitiveCount; }
+		inline int16_t getArrayCount() { return arrayCount; }
+		inline int16_t getStringCount() { return stringCount; }
+		inline int16_t getObjectCount() { return objectCount; }
 
-		Root* findByName(std::string name);
-		
+
+		Primitive findPrimitiveByName(std::string name)
+		{
+			for (Primitive p : primitives)
+			{
+				if (p.getName() == name)
+				{
+					return p;
+				}
+			}
+		}
+
+		Root* findByName(std::string name)
+		{
+			for (Primitive p : primitives)
+			{
+				if (p.getName() == name)
+				{
+					return dynamic_cast<Root*>(&p);
+				}
+			}
+
+			for (Array arr : arrays)
+			{
+				if (arr.getName() == name)
+				{
+					return dynamic_cast<Root*>(&arr);
+				}
+			}
+
+			for (Array str : strings)
+			{
+				if (str.getName() == name)
+				{
+					return  dynamic_cast<Root*>(&str);
+				}
+			}
+
+			for (Object o : objects)
+			{
+				if (o.getName() == name)
+				{
+					return  dynamic_cast<Root*>(&o);
+				}
+			}
+
+			std::cout << "no as such" << std::endl;
+
+			return new Object("SYSTEM:empty");
+
+		}
 	};
 
 }
