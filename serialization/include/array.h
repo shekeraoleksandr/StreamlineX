89c66fc4115bb3a7a83a8865ff6337402046ed53
Oleksandr Shekera <oleksandrshekera@gmail.com>
2023-05-13 13:55:32 +0300

    refactor

--- serialization/include/array.h
+++ serialization/include/array.h
@@ -1,58 +1,60 @@
 #pragma once
+
 #include "root.h"
 #include <memory>
 #include "core.h"
 
 
-namespace ObjectModel
-{
-	class Array : public Root
-	{
-	private:
-		uint8_t type = 0;
-		static int32_t count;
-		std::vector<uint8_t>* data = nullptr;
-	public:
-		Array();
-	public:
-		template<typename T>
-		static std::unique_ptr<Array> createArray(std::string name, Type type, std::vector<T> value)
-		{
-			std::unique_ptr<Array> arr = std::make_unique<Array>();
-			arr->setName(name);
-			arr->wrapper = static_cast<uint8_t>(Wrapper::ARRAY);
-			arr->type = static_cast<uint8_t>(type);
-			arr->count = (int32_t)value.size();
-			arr->data = new std::vector<uint8_t>(sizeof(T) * arr->count);
-			arr->size += (int32_t)(value.size()) * sizeof( T);
-			int16_t iterator = 0;
-			Core::template encode<T>(*arr->data, iterator, value);
+namespace ObjectModel {
+    class Array : public Root {
+    private:
+        uint8_t type = 0;
+        static int32_t count;
+        std::vector<uint8_t> *data = nullptr;
+    public:
+        Array();
+
+    public:
+        template<typename T>
+        static std::unique_ptr<Array> createArray(std::string name, Type type, std::vector<T> value) {
+            std::unique_ptr<Array> arr = std::make_unique<Array>();
+            arr->setName(name);
+            arr->wrapper = static_cast<uint8_t>(Wrapper::ARRAY);
+            arr->type = static_cast<uint8_t>(type);
+            arr->count = (int32_t) value.size();
+            arr->data = new std::vector<uint8_t>(sizeof(T) * arr->count);
+            arr->size += (int32_t) (value.size()) * sizeof(T);
+            int16_t iterator = 0;
+            Core::template encode<T>(*arr->data, iterator, value);
 
 
-			return arr;
-		}
+            return arr;
+        }
 
-		template<typename T>
-		static std::unique_ptr<Array> createString(std::string name, Type type, T value)
-		{
-			std::unique_ptr<Array>str = std::make_unique<Array>();
-			str->setName(name);
-			str->wrapper = static_cast<uint8_t>(Wrapper::STRING);
-			str->type = static_cast<uint8_t>(type);
-			str->count = (int32_t)value.size();
-			str->data = new std::vector<uint8_t>(value.size());
-			str->size += (int32_t)value.size();
-			int16_t iterator = 0;
-			Core::template encode<T>(*str->data, iterator, value);
+        template<typename T>
+        static std::unique_ptr<Array> createString(std::string name, Type type, T value) {
+            std::unique_ptr<Array> str = std::make_unique<Array>();
+            str->setName(name);
+            str->wrapper = static_cast<uint8_t>(Wrapper::STRING);
+            str->type = static_cast<uint8_t>(type);
+            str->count = (int32_t) value.size();
+            str->data = new std::vector<uint8_t>(value.size());
+            str->size += (int32_t) value.size();
+            int16_t iterator = 0;
+            Core::template encode<T>(*str->data, iterator, value);
 
 
-			return str;
-		}
+            return str;
+        }
 
-		void pack(std::vector<uint8_t>&, int16_t&);
-		static Array unpack(std::vector<uint8_t>& buffer, int16_t&);
-		static Array unpackS(std::vector<uint8_t>& buffer, int16_t&);
+        void pack(std::vector<uint8_t> &, int16_t &);
+
+        static Array unpack(std::vector<uint8_t> &buffer, int16_t &);
+
+        static Array unpackS(std::vector<uint8_t> &buffer, int16_t &);
 
         std::vector<uint8_t> getData();
-	};
+
+        inline std::vector<uint8_t> *getPtrData() { return data; }
+    };
 }
--- serialization/include/primitive.h
+++ serialization/include/primitive.h
@@ -1,38 +1,40 @@
 #pragma once
+
 #include "root.h"
 #include <memory>
 #include "core.h"
 
-namespace ObjectModel
-{
-	class Primitive : public Root
-	{
-	private:
-		uint8_t type = 0;
-		std::vector<uint8_t>* data = nullptr;
-	public:
-		Primitive();
-	public:
-		template<typename T>
-		static std::unique_ptr<Primitive> create(std::string name, Type type, T value)
-		{
-			std::unique_ptr<Primitive> p = std::make_unique<Primitive>();
-			p->setName(name);
-			p->wrapper = static_cast<uint8_t>(Wrapper::PRIMITIVE);
-			p->type = static_cast<uint8_t>(type);
-			p->data = new std::vector<uint8_t>(sizeof value);
-			p->size += (int32_t)p->data->size();
-			int16_t iterator = 0;
-			Core::template encode<T>(*p->data, iterator, value);
+namespace ObjectModel {
+    class Primitive : public Root {
+    private:
+        uint8_t type = 0;
+        std::vector<uint8_t> *data = nullptr;
+    public:
+        Primitive();
+
+    public:
+        template<typename T>
+        static std::unique_ptr<Primitive> create(std::string name, Type type, T value) {
+            std::unique_ptr<Primitive> p = std::make_unique<Primitive>();
+            p->setName(name);
+            p->wrapper = static_cast<uint8_t>(Wrapper::PRIMITIVE);
+            p->type = static_cast<uint8_t>(type);
+            p->data = new std::vector<uint8_t>(sizeof value);
+            p->size += (int32_t) p->data->size();
+            int16_t iterator = 0;
+            Core::template encode<T>(*p->data, iterator, value);
 
 
-			return p;
-		}
+            return p;
+        }
 
-		void pack(std::vector<uint8_t>&, int16_t&);
-		static Primitive unpack(const std::vector<uint8_t>&, int16_t&);
-		std::vector<uint8_t> getData();
-		inline std::vector<uint8_t>* getPtrData() { return data; }
-	};
+        void pack(std::vector<uint8_t> &, int16_t &);
+
+        static Primitive unpack(const std::vector<uint8_t> &, int16_t &);
+
+        std::vector<uint8_t> getData();
+
+        inline std::vector<uint8_t> *getPtrData() { return data; }
+    };
 
 }
